1346036cf05df295322f7444bbbab81db51a2e2a
Suresh S <rocky@localhost.localdomain>
2026-02-24 21:26:10 +0530

    Server Port Change

--- src/main/java/com/suresh/jenkins_demo/HelloController.java
+++ src/main/java/com/suresh/jenkins_demo/HelloController.java
@@ -1,4 +1,5 @@
 package com.suresh.jenkins_demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello DevOps! Jenkins is working 🚀";
+        return "Congrats Suresh, You did it!";
     }
 }
