27a21ab6709552cd4753101a6359c45b9c1852a5
Suresh S <rocky@localhost.localdomain>
2026-02-24 20:22:13 +0530

    Initial Spring Boot App for Jenkins

---
+++ src/main/java/com/suresh/jenkins_demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.suresh.jenkins_demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello DevOps! Jenkins is working 🚀";
+    }
+}
